50975c985ae5d69d88e5e6e4d9a1e155f28e7568
siiine-764 <yassinepro764@gmail.com>
2023-11-21 22:28:03 +0100

    update

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-const app = express();
 //lRBXb6e6btrNGSvQ
 // yassinepro764
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.........");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected!");
+});
+
+
 app.listen(8800, () => {
-    console.log("Connected to backend..x.......");
+    connect();
+    console.log("Connected to backend");
   });
