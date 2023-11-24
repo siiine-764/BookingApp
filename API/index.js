b5815174fceb2a7f169d0fab89f27dc2a4fe4a43
siiine-764 <yassinepro764@gmail.com>
2023-11-24 22:46:50 +0100

    ADD read-json

--- API/index.js
+++ API/index.js
@@ -6,17 +6,58 @@ import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import usersRoute from "./routes/users.js";
 
-
 //lRBXb6e6btrNGSvQ
 // yassinepro764
 
+import { promises as fsPromises } from 'fs';
+
 const app = express();
+const port = 8800;
+
+
+// Read JSON file
+app.get('/read-json', async (req, res) => {
+  try {
+    const data = await fsPromises.readFile('data.json', 'utf8');
+    const jsonData = JSON.parse(data);
+    res.json(jsonData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error reading JSON file');
+  }
+});
+
+
+// Edit JSON file
+// app.post('/edit-json', async (req, res) => {
+//   try {
+//     // Read the existing data
+//     const data = await fsPromises.readFile('data.json', 'utf8');
+//     const jsonData = JSON.parse(data);
+
+//     // Modify the data (for example, add a new user)
+//     const newUser = req.body; // Assuming the request body contains the new user data
+//     jsonData.users.push(newUser);
+
+//     // Write the modified data back to the file
+//     await fsPromises.writeFile('data.json', JSON.stringify(jsonData, null, 2), 'utf8');
+
+//     res.json({ success: true, message: 'JSON file updated successfully' });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Error editing JSON file');
+//   }
+// });
+
+
+
+// const app = express();
 dotenv.config();
 
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-    console.log("Connected to mongoDB....");
+    console.log("Connected to mongoDB.... ++");
   } catch (error) {
     throw error;
   }
@@ -30,7 +71,8 @@ mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected!");
 });
  
-app.listen(8800, () => {
+app.listen(port, () => {
     connect();
+    console.log(`Server is running on port ${port}...`);
     console.log("Connected to backend");
   });
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # BookingApp
 
+what you needed to run this project:
+    
+    npm init -y
+
+    npm add express
+
+    npm add nodemon
+
+    npm add dotenv
+
+    npm add mongoose
+
 expressjs:
 https://expressjs.com/en/starter/installing.html
