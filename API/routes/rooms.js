e0d73696f310389b0e9ecec4a854776c6c86144a
siiine-764 <yassinepro764@gmail.com>
2023-11-30 16:09:14 +0100

    Add class room

--- API/routes/rooms.js
+++ API/routes/rooms.js
@@ -1,11 +1,22 @@
 import express from "express";
+import Room from "../models/Room.js";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) =>{
-    res.send("router rooms");
-    // console.log("Im a rooms"); 
-})
+//CREATE
+router.post("/", createRoom);
 
+//UPDATE
+router.put("/:id", updateRoom);
+
+//DELETE
+router.delete("/:id", deleteRoom);
+
+//GET
+router.get("/:id", getRoom);
+
+//GET ALL
+router.get("/", getRooms);
 
 export default router
